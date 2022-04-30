1b7abd4c5924f420af6e46514a27bcbbcb8a2360
akshay4180 <akshaykaduba.kate@gmail.com>
2022-04-30 16:58:35 +0530

    my first commit

---
+++ src/github/GitHubV1.java
@@ -0,0 +1,17 @@
+package github;
+
+public class GitHubV1 {
+
+	public static void main(String[] args) {
+		int a=18;
+		if(a>=18) {
+			System.out.println("please cast your vote");
+		}
+		
+		else {
+			System.out.println("please dont cast your vote");
+		}
+
+	}
+
+}
