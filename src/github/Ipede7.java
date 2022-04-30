dfa11529b40d9ddc76cf14fced2ba2a09f5e2367
akshay4180 <104622893+akshay4180@users.noreply.github.com>
2022-04-30 05:28:48 -0700

    tester commit..

---
+++ src/github/Ipede7.java
@@ -0,0 +1,20 @@
+package github;
+
+public class Ipede7 {
+
+	public static void main(String[] args) {
+		
+			String a="yes";
+			if(a=="yes") {
+				System.out.println("you can drive");
+			}
+			
+			else {
+				System.out.println("you can not drive");
+			}
+
+		}
+
+	}
+
+
